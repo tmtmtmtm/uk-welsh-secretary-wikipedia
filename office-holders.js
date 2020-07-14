78bfa11406b8f09a37286e22dd06473c5ecb5dee
Tony Bowden <tony@tmtm.com>
2020-07-14 20:31:14 +0100

    Initial wikidata.json

---
+++ office-holders.js
@@ -0,0 +1,17 @@
+// List of P39 data for this position. Fetch with:
+//     wd sparql office-holders.sparql Q<office id> | tee wikidata.json
+
+module.exports = office => `
+  SELECT ?statement ?item ?itemLabel ?replaces ?replacesLabel ?replacedBy ?replacedByLabel ?start ?end WHERE {
+    ?item wdt:P31 wd:Q5; p:P39 ?statement.
+    ?statement ps:P39 wd:${office}.
+    MINUS { ?statement wikibase:rank wikibase:DeprecatedRank. }
+
+    OPTIONAL { ?statement pq:P580 ?start }
+    OPTIONAL { ?statement pq:P582 ?end }
+    OPTIONAL { ?statement pq:P1365 ?replaces }
+    OPTIONAL { ?statement pq:P1366 ?replacedBy }
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  }
+  ORDER BY ?start
+`
