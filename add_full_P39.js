9c232d5d8a23762dd74cc5748730f250c3bad2d8
Tony Bowden <tony@tmtm.com>
2020-07-14 22:05:21 +0100

    Allow some parameters to be empty

--- add_full_P39.js
+++ add_full_P39.js
@@ -1,18 +1,24 @@
-module.exports = (id, startdate, enddate, replaces, replacedby) => ({
-  id,
-  claims: {
-    P39: {
-      value: 'Q1846384', // position held: Welsh Secretary
-      qualifiers: {
-        P580: startdate,
-        P582: enddate,
-        P1365: replaces,
-        P1366: replacedby
-      },
-      references: {
-        P143: 'Q328', // enwiki
-        P4656: 'https://en.wikipedia.org/wiki/Secretary_of_State_for_Wales' // import URL
-      },
+module.exports = (id, startdate, enddate, replaces, replacedby) => {
+  const qualifiers = { }
+
+  if (startdate)  qualifiers['P580']  = startdate
+  if (enddate)    qualifiers['P582']  = enddate
+  if (replaces)   qualifiers['P1365'] = replaces
+  if (replacedby) qualifiers['P1366'] = replacedby
+
+  if (startdate && enddate && (startdate > enddate)) throw new Error(`Invalid dates: ${startdate} / ${enddate}`)
+
+  return {
+    id,
+    claims: {
+      P39: {
+        value: 'Q1846384', // position held: Welsh Secretary
+        qualifiers: qualifiers,
+        references: {
+          P143: 'Q328', // enwiki
+          P4656: 'https://en.wikipedia.org/wiki/Secretary_of_State_for_Wales' // import URL
+        },
+      }
     }
   }
-})
+}
